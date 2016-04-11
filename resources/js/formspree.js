c6246e5a024a8a7a3488bf7b9c44e08c04474f44
RazvanGabriel <razvan.gabriel@hotmail.com>
2016-04-11 18:17:03 +0300

    fixes #12
    Whenever a confirmation message appears the script makes sure to hide
    the other message before displaying.

--- resources/js/formspree.js
+++ resources/js/formspree.js
@@ -12,11 +12,13 @@ $contactForm.submit(function(e) {
 		success: function(data) {
 			/*$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert--success">Message sent!</div>');*/
+            $('.error').css("display","none");
             $('.succes').css("display","block");
 		},
 		error: function(err) {
 			/*$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');*/
+            $('.succes').css("display","none");
             $('.error').css("display","block");
 		}
 	});
